113470fcbca463775ab8b351fcab0820dc37dfa6
Jie317 <jie.he@mines-ales.org>
2017-03-18 10:45:58 +0100

    Update

--- quick_push.sh
+++ quick_push.sh
@@ -11,3 +11,12 @@ else
 	git commit -m "$*"
 fi
 git push origin master
+
+
+# Other tips
+# 1 ssh login: https://help.github.com/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/ then run: ssh-keyscan -t rsa 
+github.com >> ~/.ssh/known_hosts
+# 2 init a repo: 
+# git init && git remote add origin git@github.com:Jie317/***.git
+# git config --global user.name "Your name here"
+# git config --global user.email "your_email@example.com"
